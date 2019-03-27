2b5768a2d2b3cf32efcc345e643a2b1941406f6b
Jake Bartlow <jtbartlow@gmail.com>
2019-03-27 18:50:37 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,10 +3,10 @@ def reformat_languages(languages)
    languages.each do |style, description|
     description.each do |language_name, type|
       if new_hash.has_key?(language_name)
-        new_hash[lang][:style] << style
+        new_hash[language_name][:style] << style
       else
-        new_hash[lang] = type
-        new_hash[lang][:style] = [style]
+        new_hash[language_name] = type
+        new_hash[language_name][:style] = [style]
       end
     end
   end
